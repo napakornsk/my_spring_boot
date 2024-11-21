3ed1b56b7d84262c84d1f8867baa2e95e7e2b353
napakornsk <105049207+napakornsk@users.noreply.github.com>
2024-11-21 19:47:16 +0700

    Add files via upload

---
+++ student/src/test/java/rest/student/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
